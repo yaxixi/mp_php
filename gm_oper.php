8a81da9a2a7eb03108d9ab64276b8649cd3da398
yaxixi <cx0cx2001@aliyun.com>
2018-03-24 12:24:46 +0800

    代码提交

---
+++ gm_oper.php
@@ -0,0 +1,52 @@
+<?php
+
+	/*error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+    ini_set('display_startup_errors','On');
+    ini_set('error_log', dirname(__FILE__) . '/error_log.txt');*/
+
+    error_reporting(0);
+
+    include_once "common/log.php";
+    include_once "common/ez_sql_mysql.php";
+    require_once('common/SnsNetwork.php');
+
+    function go_error($ret, $msg)
+    {
+        addLog("gm_oper", $msg . " " . $ret);
+        die($ret);
+    }
+
+    $db = ezSQL_mysql::get_db("mpay");
+    if (!$db)
+    {
+        $return['msg'] = '数据库连接失败';
+        $return['ret'] = -1;
+        go_error(json_encode($return), "fail to connect dbasebase");
+    }
+
+    function switch_account_status()
+    {
+        $account = $_REQUEST["account"];
+        $status = $_REQUEST["status"];
+        global $db;
+        $db->query("update account set status=$status where account='$account'");
+
+        die(json_encode(array('ret'=>0,'msg'=>'OK')));
+    }
+
+    function return_money()
+    {
+        $id = $_REQUEST['id'];
+        global $db;
+        $db->query("update charge_exception set status=2 where id=$id and status=0");
+
+        die(json_encode(array('ret'=>0,'msg'=>'OK')));
+    }
+
+    $func = $_REQUEST["func"];
+    if ($func == "switch_account_status")
+        switch_account_status();
+    else if ($func == "return_money")
+        return_money();
+?>
